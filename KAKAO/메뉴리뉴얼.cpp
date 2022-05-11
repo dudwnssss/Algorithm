40cffd8710ab6b686f691da1c5cda74b7b4f0ff2
dudwnssss <dudwnssss@naver.com>
2022-05-11 10:42:39 +0900

    지금까지 한거 정리

--- KAKAO/메뉴리뉴얼.cpp
+++ KAKAO/메뉴리뉴얼.cpp
@@ -1,4 +1,41 @@
-//
-// Created by 임영준 on 2022/05/10.
-//
-
+//#include <string>
+//#include <vector>
+//#include <map>
+//#include <algorithm>
+//#include <iostream>
+//using namespace std;
+//bool cmp (pair<string,int>a,pair<string,int>b){
+//    return a.second>b.second;
+//}
+//vector<string> solution(vector<string> orders, vector<int> course) {
+//    vector<string> answer;
+//    for(int i=0; i<course.size(); i++){
+//        map <string, int> m;
+//        for(int j=0; j<orders.size(); j++){
+//            string s= orders[j];
+//            if(course[i]>s.size())continue;
+//            sort(s.begin(),s.end());
+//            vector <bool> check (s.size(),0);
+//            for(int k=s.size()-course[i]; k<s.size(); k++) check[k]=1;
+//            do{
+//                string s1 ="";
+//                for(int k=0; k<s.size(); k++){
+//                    if(check[k])s1+=s[k];
+//                }
+//                m[s1]++;
+//            }while(next_permutation(check.begin(),check.end()));
+//        }
+//        if(m.empty())continue;
+//        vector <pair<string, int>> v(m.begin(),m.end());
+//        sort(v.begin(),v.end(), cmp);
+//        if(v.begin()->second==1)continue;
+//        for (auto x : v)cout<< x.first <<" "<<x.second<<endl;
+//        int compare = v.begin()->second;
+//        for(auto x : v){
+//            if(x.second==compare)answer.push_back(x.first);
+//            else break;
+//        }
+//    }
+//    sort(answer.begin(),answer.end());
+//    return answer;
+//}
--- KAKAO/컬러링북.cpp
+++ KAKAO/컬러링북.cpp
@@ -2,8 +2,7 @@
 //
 //using namespace std;
 //int bfs(int m, int n, int x, int y, vector<vector<int>>& picture, int target);
-//
-//// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+
 //vector<int> solution(int m, int n, vector<vector<int>> picture) {
 //    int number_of_area = 0;
 //    int max_size_of_one_area = 0;
